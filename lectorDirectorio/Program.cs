91fe2d18ff6c45c30e0e08ed8440559052d9af23
Luis7l <luisledesma2509@gmail.com>
2025-06-10 15:37:41 -0900

    primeros cambios

---
+++ lectorDirectorio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+    
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        string? path;
+        bool Estexto = false;
+
+        do
+
+        {
+            Console.WriteLine("Ingrese el path de  el archivo ");
+            path = Console.ReadLine();
+            if (Directory.Exists(path))
+            {
+                Estexto = true;
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un Path valido");
+                path = Console.ReadLine();
+            }
+        } while (!Estexto);
+        
+    }
+}
